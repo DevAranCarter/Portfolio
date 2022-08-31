2918e879e9a9d7146e3a780025d09373a0743124
Devin Carter <devinacarter97@gmail.com>
2022-08-31 18:00:59 +0100

    Fixed color issues

--- portfolio/components/chakra.tsx
+++ portfolio/components/chakra.tsx
@@ -6,12 +6,23 @@ import {
   } from '@chakra-ui/react'
 import theme from '../components/theme'
   
-  export default function Chakra({}) {
-    const colorModeManager = localStorageManager
-  
-    return (
-      <ChakraProvider theme={theme} colorModeManager={colorModeManager}>
-       
-      </ChakraProvider>
-    )
+export default function Chakra({ cookies, children }: any) {
+  const colorModeManager =
+    typeof cookies === 'string'
+      ? cookieStorageManager
+      : localStorageManager
+
+  return (
+    <ChakraProvider theme={theme} colorModeManager={colorModeManager}>
+      {children}
+    </ChakraProvider>
+  )
+}
+
+export async function getServerSideProps({ req }: any) {
+  return {
+    props: {
+      cookies: req.headers.cookie ?? ''
+    }
   }
+}
