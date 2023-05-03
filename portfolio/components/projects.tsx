4d364deb561035fb4ab3560089143296aaa00f7a
Devin Carter <devinacarter97@gmail.com>
2023-05-03 14:02:02 +0100

    updated projects

--- portfolio/components/projects.tsx
+++ portfolio/components/projects.tsx
@@ -1,21 +1,34 @@
-import { Container, Heading, SimpleGrid, Divider, Center } from '@chakra-ui/react'
-import Layout from '../components/layouts/article'
-import Section from '../components/section'
-import { WorkGridItem } from '../components/grid-item'
+import {
+  Container,
+  Heading,
+  SimpleGrid,
+  Divider,
+  Center,
+} from "@chakra-ui/react";
+import Layout from "../components/layouts/article";
+import Section from "../components/section";
+import { WorkGridItem } from "../components/grid-item";
 
 const Works = () => (
   <Layout title="Work">
-    <Container pt={{base:'650px', md:'400px', lg:'200px', xl:'100px'}}
-    textColor={"blackAlpha.900"}>
+    <Container
+      pt={{ base: "650px", md: "400px", lg: "200px", xl: "100px" }}
+      textColor={"blackAlpha.900"}
+    >
       <Center>
-      <Heading fontSize={50} mb={4} id='work'>
-        Work
-      </Heading>
+        <Heading fontSize={50} mb={4} id="work">
+          Work
+        </Heading>
       </Center>
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="NFT-template" link="https://github.com/felwintrr/fantom-dapp" title="NFT template" thumbnail={'/images/Screenshot 2022-06-02 at 14.08.41.png'}>
+          <WorkGridItem
+            id="NFT-template"
+            link="https://github.com/felwintrr/fantom-dapp"
+            title="NFT template"
+            thumbnail={"/images/Screenshot 2022-06-02 at 14.08.41.png"}
+          >
             A website that NFT artists can customise for their own NFT project.
           </WorkGridItem>
         </Section>
@@ -23,17 +36,36 @@ const Works = () => (
           <WorkGridItem
             id="Google-Homepage"
             title="Google Homepage recreation"
-            thumbnail={'/images/Screenshot 2022-06-02 at 14.13.03.png'}
+            thumbnail={"/images/Screenshot 2022-06-02 at 14.13.03.png"}
             link="https://github.com/felwintrr/fantom-dapp"
           >
             A recreation of the Google Homepage
           </WorkGridItem>
         </Section>
+        <Section>
+          <WorkGridItem
+            id="NoMoreWaste"
+            title="NoMoreWaste Application"
+            thumbnail={"/images/Screenshot 2023-05-03 at 13.50.01"}
+            link="https://github.com/jannikuhl/NoMoreWaste"
+          >
+            An app designed to reduce food waste{" "}
+          </WorkGridItem>
+        </Section>
 
+        <Section>
+          <WorkGridItem
+            id="Food pricing app"
+            title="Dish pricing application"
+            thumbnail={"/images/Screenshot 2023-05-03 at 13.56.25"}
+          >
+            An app that allows users to effectively price their dishes.{" "}
+          </WorkGridItem>
+        </Section>
       </SimpleGrid>
     </Container>
   </Layout>
-)
+);
 
-export default Works
-export { getServerSideProps } from '../components/chakra'
+export default Works;
+export { getServerSideProps } from "../components/chakra";
